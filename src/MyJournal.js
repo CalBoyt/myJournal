9843d859baab5e91b09f50a05dc139749bfb93c8
Caleen Boyter <caleenboyter@gmail.com>
2024-01-29 19:10:21 -0500

    journal entries

---
+++ src/MyJournal.js
@@ -0,0 +1,35 @@
+import { JournalForm } from "./JournalForm"
+import { JournalList } from "./JournalList"
+import { Weather } from "./Weather"
+
+
+export const MyJournal = () => {
+
+
+    return (
+        
+    <section className="section">
+        <div className="container">
+            <h1 className="title">
+                My Journal
+            </h1>
+            <p className="subtitle">
+                 Tell me how you really feel
+            </p>
+            <div className="columns">
+                <div className="column is-three-fifths">
+                    <JournalForm  />
+                </div>
+                <div className="column">
+                    <Weather  />
+                </div>
+            </div>
+            <JournalList />
+    
+    </div>
+    </section>
+    
+    )
+
+
+}
